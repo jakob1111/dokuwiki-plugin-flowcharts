d81453f6829de1fb6f15b72c3d5aad209238dc36
Szymon Olewniczak <solewniczak@rid.pl>
2018-06-04 16:06:37 +0200

    Adjust method signatures to match parent

--- syntax.php
+++ syntax.php
@@ -46,7 +46,7 @@ class syntax_plugin_flowcharts extends DokuWiki_Syntax_Plugin
     /**
      * Handle matches of the flowcharts syntax
      */
-    function handle($match, $state, $pos, &$handler){
+    function handle($match, $state, $pos, Doku_Handler $handler){
         switch ($state) {
             case DOKU_LEXER_ENTER:
                 $data = strtolower(trim(substr($match,6,-1)));
@@ -66,7 +66,7 @@ class syntax_plugin_flowcharts extends DokuWiki_Syntax_Plugin
     /**
      * Render xhtml output or metadata
      */
-    function render($mode, &$renderer, $indata) {
+    function render($mode, Doku_Renderer $renderer, $indata) {
         if($mode == 'xhtml'){
             list($state, $match) = $indata;
             switch ($state) {
